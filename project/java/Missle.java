256cc8373c4483e8fbd8693ca7e2019a19193495
daniel <danielguo123456@hotmail.com>
2019-07-07 21:56:20 -0700

    move files

--- project/java/Missle.java
+++ project/java/Missle.java
@@ -1,4 +1,19 @@
+// Author: Zhiyuan Guo
+
 package com.nike.spaceinvaders;
 
+import android.graphics.RectF;
+
 class Missle {
+    private RectF mRect;
+    private float mXVelocity;
+    private float mYVelocity;
+    private float mMissleWidth;
+    private float mMissleHeight;
+
+
+    // Missle constructor
+    Missle(){
+
+    }
 }
